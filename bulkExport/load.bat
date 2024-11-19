6a660209fdaa98af48fa98f02d5dce3798b6a595
ChallierFabrice <fabrice.challier@littlechaman.com>
2024-11-19 17:52:18 +0100

    Fix Load.bat

--- bulkExport/load.bat
+++ bulkExport/load.bat
@@ -1,14 +1,14 @@
 echo "-- Loading Account"
-sfdx force:data:bulk:upsert -s Account -f ./bulkExport/account.csv -i ExternalId__c  --wait 40
+call sfdx force:data:bulk:upsert -s Account -f ./bulkExport/account.csv -i ExternalId__c  --wait 40
 
 echo "-- Loading Contact
-sfdx force:data:bulk:upsert -s Contact -f ./bulkExport/contact.csv -i ExternalId__c  --wait 40 
+call sfdx force:data:bulk:upsert -s Contact -f ./bulkExport/contact.csv -i ExternalId__c  --wait 40 
 
 echo "-- Loading ObjectA
-sfdx force:data:bulk:upsert -s ObjectA__c -f ./bulkExport/objectA.csv -i Name  --wait 40 
+call sfdx force:data:bulk:upsert -s ObjectA__c -f ./bulkExport/objectA.csv -i Name  --wait 40 
 
 echo "-- Loading ObjectB
-sfdx force:data:bulk:upsert -s ObjectB__c -f ./bulkExport/objectB.csv -i Name  --wait 40 
+call sfdx force:data:bulk:upsert -s ObjectB__c -f ./bulkExport/objectB.csv -i Name  --wait 40 
 
 echo "-- Loading ObjectA
-sfdx force:data:bulk:upsert -s ObjectA__c -f ./bulkExport/objectA_AfterObjectB.csv -i Name  --wait 40 
+call sfdx force:data:bulk:upsert -s ObjectA__c -f ./bulkExport/objectA_AfterObjectB.csv -i Name  --wait 40 
